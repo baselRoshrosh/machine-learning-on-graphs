c59c98f836a75ceb3dc6794ce090adc5bc502bce
Moritz Spühler <moritzspuehler@outlook.com>
2024-12-06 17:12:35 +0100

    feat: Rudimentary Node Interface
    Bietet die essentiellen Node Funktionen

---
+++ include/Node.hpp
@@ -0,0 +1,47 @@
+#ifndef NODE_HPP
+#define NODe_HPP
+
+#include <vector>
+
+/**
+ * @class Node
+ * @brief Represents a graph node.
+ *
+ * This class is used to store the features of a node and provide an interface
+ * for accessing node-specific information, such as its ID and feature vector.
+ */
+template <typename T>
+class Node
+{
+public:
+    /**
+     * @brief Constructs a new Node object.
+     *
+     * @param id The unique identifier for the node.
+     * @param featureVector The feature vector of the node.
+     */
+    Node(int id, std::vector<T> featureVector);
+
+    /**
+     * @brief Get the ID of the node.
+     *
+     * @return int The unique identifier of the node.
+     */
+    virtual int getId() const = 0;
+
+    /**
+     * @brief Get the feature vector of the node.
+     *
+     * @return std::vector<T> The feature vector associated with the node.
+     */
+    virtual std::vector<T> getFeatureVector() const = 0;
+
+    // Virtual destructor to support polymorphism
+    virtual ~Node() = default;
+
+private:
+    int id;                  ///< unique node identifier
+    std::vector<T> features; ///< the feature vector of the nodes
+};
+
+#endif
