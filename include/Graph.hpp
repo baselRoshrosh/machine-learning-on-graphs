1aa02dd5c8c7519fca09d4e8874bf48f82225665
Johannes Storck <johannes.storck@informatik.hu-berlin.de>
2025-01-28 17:06:55 +0100

    Update IGraph.hpp to Graph.hpp
    Former-commit-id: be2d27c49d181af24bfd8a53e2f3c8269680ba61

--- include/Graph.hpp
+++ include/Graph.hpp
@@ -2,11 +2,12 @@
 #define GRAPH_HPP
 
 #include <vector>
+#include <memory>
 #include <string>
 #include <utility>
-#include "IEdges.hpp"
-#include "BasicEdges.hpp"
-#include "INode.hpp"
+#include "../include/IEdges.hpp"
+#include "../include/BasicEdges.hpp"
+#include "../include/Node.hpp"
 
 /**
  * @class Graph
@@ -16,42 +17,54 @@
  * It supports adding and querying nodes and edges, as well as parsing
  * graph data from files.
  */
-class IGraph {
+class Graph {
+private:
+    std::vector<Node> nodes; /// the vector of nodes
+    std::unique_ptr<BasicEdges> edges; /// object holding the pool of edges
+
 public:
+    /**
+     * @brief Constructs a graph by parsing from txt files.
+     *
+     * @param nodesFile The file containing node information.
+     * @param edgesFile The file containing edge information.
+     */
+    Graph(const std::string& nodesFile, const std::string& edgesFile);
+
     /**
      * @brief Retrieves all nodes in the graph.
      *
      * @return std::vector<int> A list of all node IDs.
      */
-    virtual std::vector<int> getNodes() const = 0;
+    std::vector<int> getNodes() const;
 
     /**
      * @brief Retrieves all edges in the graph.
      *
      * @return std::vector<std::pair<int, int>> A list of all edges.
      */
-    virtual std::vector<std::pair<int, int>> getEdges() const = 0;
+    std::vector<std::pair<int, int>> getEdges() const;
 
     /**
      * @brief Retrieves neighbors of a specified node.
      *
      * @return std::vector<int> A list of all neighbor node IDs.
      */
-    virtual std::vector<int> getNeighbors(int nodeId) const = 0;
+    std::vector<int> getNeighbors(int nodeId) const;
 
     /**
      * @brief Retrieves node count of graph.
      *
      * @return int Number of all the nodes in the graph.
      */
-    virtual int getNodeCount() const = 0;
+    int getNodeCount() const;
 
     /**
      * @brief Retrieves edge count of graph.
      *
      * @return int Number of all the edges in the graph.
      */
-    virtual int getEdgeCount() const = 0;
+    int getEdgeCount() const;
 
     /**
      * @brief Retrieves the feature vector of a node by its ID.
@@ -59,23 +72,15 @@ public:
      * @param nodeId The ID of the node.
      * @return std::vector<double> The feature vector of the node or an empty vector if the node is not found.
      */
-    virtual std::vector<double> getFeatureById(int nodeId) const = 0;
+    std::vector<double> getFeatureById(int nodeId) const;
 
     /**
      * @brief Update the entire feature vector of a node by its ID.
      * @param nodeId The ID of the node whose feature vector is to be updated.
      * @param newFeatures The new feature vector to set for the node.
      */
-    virtual void updateFeatureById(int nodeId, const std::vector<double>& newFeatures) = 0;
-
-    // Virtual destructor to support polymorphism
-    virtual ~IGraph() = default;
-
-private:
-    std::vector<double> nodes; /// the vector of nodes
-    std::unique_ptr<IEdges> edges; /// object holding the pool of edges
+    void updateFeatureById(int nodeId, const std::vector<double>& newFeatures);
 };
 
-#include "../src/Graph.tpp"
 
 #endif
