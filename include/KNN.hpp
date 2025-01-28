f86beedb9f88fa76850295bcc92c44009b4f84c0
Johannes Storck <johannes.storck@informatik.hu-berlin.de>
2025-01-28 17:05:25 +0100

    Update KNN.hpp
    Former-commit-id: a18670f919b0a16d569357f3432650c1c8c8fc5c

--- include/KNN.hpp
+++ include/KNN.hpp
@@ -12,26 +12,44 @@
 #include <limits>
 #include <iomanip>
 #include <fstream>
-#include "../include/IGraph.hpp"
+#include "../include/Graph.hpp"
 #include "../src/BasicEdges.cpp"
 
-
+/**
+ * @class KNN
+ * @brief Implementation of the K-Nearest Neighbors algorithm for feature estimation.
+ */
 class KNN {
 private:
     //Cache for neighbors and path to avoid repeatedly calculating them
     std::unordered_map<int, std::vector<int>> cachedNeighbors;          
-    std::unordered_map<int, std::unordered_map<int, int>> precomputedPaths; 
+    std::unordered_map<int, std::unordered_map<int, int>> precomputedPaths;
 
+    /**
+     * @brief Cache the neighbors of all nodes in the graph.
+     *
+     * @param graph The graph to process.
+     */
     void cacheNeighbors(const Graph& graph);
 
-    void calcPaths(const Graph& graph);
+    /**
+     * @brief Calculate the shortest paths for all nodes up to a distance of k.
+     *
+     * @param graph The graph to process.
+     * @param k The number of nearest neighbors to consider.
+     */
+    void calcPaths(const Graph& graph, int k);
 
 public:
-
+    /**
+     * @brief Estimate missing features for nodes in the graph using k-nearest neighbors.
+     *
+     * @param graph The graph to process.
+     * @param k The number of neighbors to consider.
+     */
     void estimateFeatures(Graph& graph, int k);
 };
 
-#include "../src/KNN.tpp"
 
 #endif // KNN_HPP
 
