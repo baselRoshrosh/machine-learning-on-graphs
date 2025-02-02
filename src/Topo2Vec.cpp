208f16cc991270834594ebfcc741b16bab84b66e
baselscode <basel123r@gmail.com>
2025-02-02 15:16:44 +0100

    getSample funktion implementiert
    Former-commit-id: 36b5bac46a0e92a705c8efcda076168059b2ca72

--- src/Topo2Vec.cpp
+++ src/Topo2Vec.cpp
@@ -1,2 +1,20 @@
-#include "../include/Topo2Vec.hpp"
+#include "Topo2Vec.hpp"
 
+
+std::vector<std::vector<double>> Topo2Vec::getSample(std::vector<std::vector<double>> setOfVectors, int sampleSize)
+{
+    std::vector<std::vector<double>> sample;
+    if (setOfVectors.empty() || sampleSize <= 0){ //Out-of-bounds check
+        return sample;
+    }
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, setOfVectors.size() - 1);
+
+    for (int i = 0; i < sampleSize; i++)
+    {
+        sample.push_back(setOfVectors[dis(gen)]);
+    }
+
+    return sample;
+}
