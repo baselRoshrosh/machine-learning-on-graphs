793c3d2a293d52030873b8f7ee642bd4e30b94cb
Johannes Storck <johannes.storck@informatik.hu-berlin.de>
2025-02-18 14:21:18 +0100

    KNN.hpp
    Former-commit-id: 6d850d486bea135473bbfda24efb08fcb89fb6cc

---
+++ include/KNN.hpp
@@ -0,0 +1,79 @@
+#ifndef KNN_HPP
+#define KNN_HPP
+
+#include "IStrategies.hpp"
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <memory>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include "Graph.hpp"
+#include "BasicEdges.cpp"
+/**
+ * @class KNN
+ * @brief Implementation of the K-Nearest Neighbors algorithm for feature estimation.
+ */
+class KNN : public IStrategies {
+public:
+    /**
+     * @brief Default constructor.
+     */
+    KNN(std::shared_ptr<Graph> g) { graph = g; }
+
+    /**
+     * @brief Runs the KNN strategy.
+     */
+    void run() override;
+
+    /**
+     * @brief Extracts the results after running the strategy.
+     * @return A modified graph with missing features filled.
+     */
+    std::shared_ptr<Graph> extractResults() const override;
+
+    /**
+     * @brief Configures strategy-specific parameters.
+     * @param params A map of parameter names and their values.
+     */
+    void configure(const std::map<std::string, double>& params) override;
+
+    /**
+     * @brief Resets the strategy to its initial state.
+     */
+    void reset() override;
+
+private:
+    int k;
+    //Cache for neighbors and path to avoid repeatedly calculating them
+    std::unordered_map<int, std::vector<int>> cachedNeighbors;          
+    std::unordered_map<int, std::unordered_map<int, int>> precomputedPaths;
+
+    /**
+     * @brief Cache the neighbors of all nodes in the graph.
+     *
+     * @param graph The graph to process.
+     */
+    void cacheNeighbors(const Graph& graph);
+
+    /**
+     * @brief Calculate the shortest paths for all nodes up to a distance of k.
+     *
+     * @param graph The graph to process.
+     * @param k The number of nearest neighbors to consider.
+     */
+    void calcPaths(const Graph& graph, int k);
+
+    /**
+     * @brief Estimate missing features for nodes in the graph using k-nearest neighbors.
+     *
+     * @param graph The graph to process.
+     * @param k The number of neighbors to consider.
+     */
+    void estimateFeatures(Graph& graph, int k);
+};
+
+
+#endif // KNN_HPP
+
