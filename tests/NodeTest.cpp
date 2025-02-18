0ca453bd26376ee6a4bce6aeca2a27c6201e1ce7
Johannes Storck <johannes.storck@informatik.hu-berlin.de>
2025-02-18 10:35:44 +0100

    Update NodeTest.cpp
    Former-commit-id: 4d5a64c9308d798d5026ca8a5f39d186af531f34

--- tests/NodeTest.cpp
+++ tests/NodeTest.cpp
@@ -1,21 +1,21 @@
 #include <gtest/gtest.h>
-#include "INode.hpp"
+#include "Node.hpp"
 
 TEST(NodeTest, Constructor) {
-    Node<float> node(1, {0.5, 1.5, 2.5}, 42);
+    Node node(1, {0.5, 1.5, 2.5}, 42);
     EXPECT_EQ(node.getId(), 1);
     EXPECT_EQ(node.getFeatureVector(), std::vector<float>({0.5, 1.5, 2.5}));
     EXPECT_EQ(node.getLabel(), 42);
 }
 
 TEST(NodeTest, Getters) {
-    Node<int> node(2, {1, 2, 3}, 7);
+    Node node(2, {1, 2, 3}, 7);
     EXPECT_EQ(node.getId(), 2);
     EXPECT_EQ(node.getFeatureVector(), std::vector<int>({1, 2, 3}));
     EXPECT_EQ(node.getLabel(), 7);
 }
 
 TEST(NodeTest, DefaultConstructor) {
-    Node<double> node;
+    Node node;
     EXPECT_TRUE(node.getFeatureVector().empty());
 }
