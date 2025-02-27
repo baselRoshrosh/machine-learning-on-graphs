307ba1088ba1c416426cb131e556fb48d9daa166
spuehlem <spuehlem@informatik.hu-berlin.de>
2025-02-27 08:58:38 +0100

    fix: calculateWeightMatrix()
    Former-commit-id: bb9a0997b8e99bc3acdce2c17383dcfed5fc4635

--- src/AttributedDeepwalk.cpp
+++ src/AttributedDeepwalk.cpp
@@ -1,1 +1,22 @@
 #include "AttributedDeepwalk.hpp"
+
+void AttributedDeepwalk::calculateWeightMatrix() {
+    double weight;
+
+    for (auto [source, destination] : graph->getEdges()) {
+        weight = fusionCoefficient * measuring_attribute_similarity(source, destination) + (1 - fusionCoefficient) * measuring_structural_similarity(source, destination);
+        graph->setEdgeWeight(source, destination, weight);
+    }
+}
+
+std::unordered_map<int, std::vector<std::pair<double, size_t>>> getAliasTables() {
+    
+}
+
+double AttributedDeepwalk::measuring_attribute_similarity(int node1, int node2) const {
+
+}
+
+double AttributedDeepwalk::measuring_structural_similarity(int node1, int node2) const{
+
+}
