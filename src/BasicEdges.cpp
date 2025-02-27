3193fba1c551bf7f634478d25f7bfe8283be5fbd
spuehlem <spuehlem@informatik.hu-berlin.de>
2025-02-27 08:57:19 +0100

    fix: get updated file Graph with weight functions
    Former-commit-id: 280cf402445b031830b68c902200b5b497b5117a

--- src/BasicEdges.cpp
+++ src/BasicEdges.cpp
@@ -1,58 +1,119 @@
 #include "BasicEdges.hpp"
 
+#include <limits>
+
 // Constructor: Initializes the BasicEdges with a set of edges.
 BasicEdges::BasicEdges(const std::vector<std::pair<int, int>> &initialEdges)
-    : edges(initialEdges) {}
+{
+    for (const auto &edge : initialEdges)
+    {
+        edges.emplace_back(edge.first, edge.second, std::numeric_limits<double>::quiet_NaN());
+    }
+}
 
 // adds edge to edges-vector
 void BasicEdges::addEdge(int source, int destination)
 {
-    edges.emplace_back(source, destination);
+    if (destination < source)
+    {
+        std::swap(source, destination);
+    }
+
+    edges.emplace_back(source, destination, std::numeric_limits<double>::quiet_NaN());
 }
-
-
 
 // Retrieves the neighbors of a given node.
 std::vector<int> BasicEdges::getNeighbors(int nodeID)
 {
     std::vector<int> neighbors;
+    int start, dest;
 
     for (const auto &edge : edges)
     {
-        if (edge.first == nodeID)
+        start = std::get<0>(edge);
+        dest = std::get<1>(edge);
+
+        if (start == nodeID)
         {
-            neighbors.push_back(edge.second);
+            neighbors.push_back(dest);
         }
-        else if (edge.second == nodeID)
+        else if (dest == nodeID)
         {
-            neighbors.push_back(edge.first);
+            neighbors.push_back(start);
         }
     }
 
     return neighbors;
 }
 
-
 // Checks if an edge exists between two nodes.
 bool BasicEdges::isEdge(int source, int destination)
 {
+    if (destination < source)
+    {
+        std::swap(source, destination);
+    }
+
     for (const auto &edge : edges)
     {
-        if ((edge.first == source && edge.second == destination) ||
-            (edge.second == source && edge.first == destination))
+        if (std::get<0>(edge) == source && std::get<1>(edge) == destination)
         {
-            return true;
+            return true; // edge is found
         }
     }
+
     return false; // Only return false after checking all edges
 }
 
 // Retrieves all edges in the graph.
 std::vector<std::pair<int, int>> BasicEdges::getEdges() const
 {
-    return edges;
+    std::vector<std::pair<int, int>> edgesWithoutWeights;
+
+    for (const auto &edge : edges)
+    {
+        edgesWithoutWeights.emplace_back(std::get<0>(edge), std::get<1>(edge));
+    }
+
+    return edgesWithoutWeights;
 }
 
+// sets weight of an edge
+void BasicEdges::setWeight(int source, int destination, double weight)
+{
+    if (destination < source)
+    {
+        std::swap(source, destination);
+    }
+
+    auto it = std::find_if(edges.begin(), edges.end(), [&](const std::tuple<int, int, double> &edge)
+                           { return std::get<0>(edge) == source && std::get<1>(edge) == destination; });
+
+    if (it != edges.end())
+    {
+        std::get<2>(*it) = weight;
+    }
+}
+
+// gets weight of an edge in graph
+double BasicEdges::getWeight(int source, int destination) const
+{   
+    if (destination < source)
+    {
+        std::swap(source, destination);
+    }
+    
+    auto it = std::find_if(edges.begin(), edges.end(),
+                           [&](const std::tuple<int, int, double> &edge)
+                           { return std::get<0>(edge) == source && std::get<1>(edge) == destination; });
+
+    if (it != edges.end())
+    {
+        return std::get<2>(*it);
+    }
+
+    return std::numeric_limits<double>::quiet_NaN(); // return standard value if not found
+}
 
 // Retrieves number of edges
 int BasicEdges::size()
