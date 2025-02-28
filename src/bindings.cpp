b382ce84fb019a7f4c2e26f1738286a6cd145ed5
Johannes Storck <johannes.storck@informatik.hu-berlin.de>
2025-02-28 14:27:45 +0100

    Update bindings.cpp
    Former-commit-id: f112b2e15a34507d148cf141a28cae7f02936511

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -8,29 +8,29 @@
 namespace py = pybind11;
 
 PYBIND11_MODULE(simProject, m) {
-    m.doc() = "Python Bindings für Attributed DeepWalk, kNN und Topo2Vec";
+    m.doc() = "Python Bindings for Attributed DeepWalk, kNN and Topo2Vec";
 
     // Klasse AttributedDeepwalk
     py::class_<AttributedDeepwalk>(m, "AttributedDeepwalk")
         .def(py::init<std::shared_ptr<Graph>>())  // Falls du einen Konstruktor mit Graph nutzt
-        .def("run", &AttributedDeepwalk::run, "Führt Attributed DeepWalk aus")
-        .def("extract_results", &AttributedDeepwalk::extractResults, "Extrahiert die Ergebnisse")
-        .def("configure", &AttributedDeepwalk::configure, "Konfiguriert die Strategie")
-        .def("reset", &AttributedDeepwalk::reset, "Setzt den Zustand zurück");
+        .def("run", &AttributedDeepwalk::run, "runsAttributed DeepWalk")
+        .def("extract_results", &AttributedDeepwalk::extractResults, "extracs results")
+        .def("configure", &AttributedDeepwalk::configure, "configure parameters")
+        .def("reset", &AttributedDeepwalk::reset, "resets all configurations");
 
     // Klasse KNN
     py::class_<KNN>(m, "KNN")
         .def(py::init<std::shared_ptr<Graph>>())  // Falls du einen Konstruktor mit Graph nutzt
-        .def("run", &KNN::run, "Führt kNN-Suche aus")
-        .def("extract_results", &KNN::extractResults, "Extrahiert die Ergebnisse")
-        .def("configure", &KNN::configure, "Konfiguriert kNN-Parameter")
-        .def("reset", &KNN::reset, "Setzt den Zustand zurück");
+        .def("run", &KNN::run, "runs kNN")
+        .def("extract_results", &KNN::extractResults, "extracs results")
+        .def("configure", &KNN::configure, "configure kNN-parameters")
+        .def("reset", &KNN::reset, "resets all configurations");
 
     // Klasse Topo2Vec
     py::class_<Topo2Vec>(m, "Topo2Vec")
         .def(py::init<std::shared_ptr<Graph>>())  // Falls du einen Konstruktor mit Graph nutzt
         .def("run", &Topo2Vec::run, "Führt Topo2Vec aus")
-        .def("extract_results", &Topo2Vec::extractResults, "Extrahiert die Ergebnisse")
-        .def("configure", &Topo2Vec::configure, "Konfiguriert Topo2Vec-Parameter")
-        .def("reset", &Topo2Vec::reset, "Setzt den Zustand zurück");
+        .def("extract_results", &Topo2Vec::extractResults, "extracs results")
+        .def("configure", &Topo2Vec::configure, "configure parameters")
+        .def("reset", &Topo2Vec::reset, "resets all configurations");
 }
