2cee7b904aa1df0d06b372bfff467ff25cbbdb04
Johannes Storck <johannes.storck@informatik.hu-berlin.de>
2025-03-01 22:43:07 +0100

    Update bindings.cpp fixes the graph constructors
    Former-commit-id: 75b5c80b7063dcba412933837ec6775d10a038f2

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -13,27 +13,26 @@ PYBIND11_MODULE(semProject, m) {
     m.doc() = "Python Bindings for Attributed DeepWalk, kNN and Topo2Vec";
 
     py::class_<Graph, std::shared_ptr<Graph>>(m, "Graph")
-        .def(py::init<>())  
-        .def("load_from_files", &Graph::loadFromFiles, "Loads graph from edge and node files.");
+        .def(py::init<const std::string&, const std::string&>(), py::arg("nodesFile"), py::arg("edgesFile"));  
 
     py::class_<AttributedDeepwalk>(m, "AttributedDeepwalk")
         .def(py::init<std::shared_ptr<Graph>>(), py::arg("graph"))
         .def("run", &AttributedDeepwalk::run, "runs Attributed DeepWalk")
-        .def("extract_results", &AttributedDeepwalk::extractResults, "extracs results")
+        .def("extract_results", &AttributedDeepwalk::extractResults, "extracts results")
         .def("configure", &AttributedDeepwalk::configure, "configure parameters")
         .def("reset", &AttributedDeepwalk::reset, "resets all configurations");
 
     py::class_<KNN>(m, "KNN")
         .def(py::init<std::shared_ptr<Graph>>(), py::arg("graph")) 
         .def("run", &KNN::run, "runs kNN")
-        .def("extract_results", &KNN::extractResults, "extracs results")
+        .def("extract_results", &KNN::extractResults, "extracts results")
         .def("configure", &KNN::configure, "configure kNN-parameters")
         .def("reset", &KNN::reset, "resets all configurations");
 
     py::class_<Topo2Vec>(m, "Topo2Vec")
         .def(py::init<std::shared_ptr<Graph>>(), py::arg("graph")) 
         .def("run", &Topo2Vec::run, "runs Topo2Vec")
-        .def("extract_results", &Topo2Vec::extractResults, "extracs results")
+        .def("extract_results", &Topo2Vec::extractResults, "extracts results")
         .def("configure", &Topo2Vec::configure, "configure parameters")
         .def("reset", &Topo2Vec::reset, "resets all configurations");
 }
