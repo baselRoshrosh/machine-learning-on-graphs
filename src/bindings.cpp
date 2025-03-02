6405713a3310ee4be6eb7c51730803cc85e6c9b8
Johannes Storck <johannes.storck@informatik.hu-berlin.de>
2025-03-02 14:44:21 +0100

    Resolve "Python Einbinden"
    Former-commit-id: 8291731cedeea3502760dcb27d36add29f3c7af6

---
+++ src/bindings.cpp
@@ -0,0 +1,38 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h> 
+#include <memory> 
+
+#include "Graph.hpp"
+#include "AttributedDeepwalk.hpp"
+#include "KNN.hpp"
+#include "Topo2Vec.hpp"
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(semProject, m) {
+    m.doc() = "Python Bindings for Attributed DeepWalk, kNN and Topo2Vec";
+
+    py::class_<Graph, std::shared_ptr<Graph>>(m, "Graph")
+        .def(py::init<const std::string&, const std::string&>(), py::arg("nodesFile"), py::arg("edgesFile"));  
+
+    py::class_<AttributedDeepwalk>(m, "AttributedDeepwalk")
+        .def(py::init<std::shared_ptr<Graph>>(), py::arg("graph"))
+        .def("run", &AttributedDeepwalk::run, "runs Attributed DeepWalk")
+        .def("extract_results", &AttributedDeepwalk::extractResults, "extracts results")
+        .def("configure", &AttributedDeepwalk::configure, "configure parameters")
+        .def("reset", &AttributedDeepwalk::reset, "resets all configurations");
+
+    py::class_<KNN>(m, "KNN")
+        .def(py::init<std::shared_ptr<Graph>>(), py::arg("graph")) 
+        .def("run", &KNN::run, "runs kNN")
+        .def("extract_results", &KNN::extractResults, "extracts results")
+        .def("configure", &KNN::configure, "configure kNN-parameters")
+        .def("reset", &KNN::reset, "resets all configurations");
+
+    py::class_<Topo2Vec>(m, "Topo2Vec")
+        .def(py::init<std::shared_ptr<Graph>>(), py::arg("graph")) 
+        .def("run", &Topo2Vec::run, "runs Topo2Vec")
+        .def("extract_results", &Topo2Vec::extractResults, "extracts results")
+        .def("configure", &Topo2Vec::configure, "configure parameters")
+        .def("reset", &Topo2Vec::reset, "resets all configurations");
+}
