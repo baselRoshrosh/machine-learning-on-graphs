264e8b1c108048800df673f593beafda43c51fc8
spuehlem <spuehlem@informatik.hu-berlin.de>
2025-03-07 13:42:07 +0100

    feat: incorporating eval script
    Former-commit-id: 078c5c457f1d622106d4e201487d3a5f615f15b0

---
+++ scripts/run_strategies_for_eval.py
@@ -0,0 +1,49 @@
+import semProject as sp
+
+import os
+import zipfile
+import shutil  # for deleting temporary files
+
+# Strategies to perform
+strategies = [sp.KNN(), sp.Topo2Vec(), sp.AttributedDeepwalk()]
+strategie_names = ['KNN', 'Topo2Vec', 'AttributedDeepwalk']
+
+# Paths
+REF_FOLDER = "./ref"
+INPUT_FOLDER = "./input"
+OUTPUT_FOLDER = "./output"
+TEMP_UNZIP_FOLDER = "./temp_unzip"
+
+if not os.path.exists(TEMP_UNZIP_FOLDER):
+        os.makedirs(TEMP_UNZIP_FOLDER)  # Create directory if it doesn't exist 
+
+# Get list of available graphs in ref (without extensions)
+graph_names = [f.split('.')[0] for f in os.listdir(REF_FOLDER) if os.path.isfile(os.path.join(REF_FOLDER, f))]
+
+# for each graph in "ref" References of eval script:
+for graph_name in graph_names:
+    print(f"Read graph: {graph_name}")
+    
+    # etracting graph files
+    with zipfile.ZipFile(os.path.join(INPUT_FOLDER, graph_name + ".zip"), 'r') as zip_ref:
+        zip_ref.extractall(TEMP_UNZIP_FOLDER)
+    
+    # performing strategies
+    for i, strategy in enumerate(strategies):
+        # 1: getting graph
+        graph = sp.Graph(os.path.join(INPUT_FOLDER, graph_name) + "_features.txt", 
+                         os.path.join(INPUT_FOLDER, graph_name) + "_edges.txt")
+
+        # 2: initializing strategies
+        x = strategy(graph)
+        
+        # 3: configure
+        
+        # 4: run and extract
+        x.run()
+        results = x.extract_results()
+        x.save_features(results, os.path.join(OUTPUT_FOLDER, f"{graph_name}_{strategie_names[i]}txt"))
+        
+     # Clean up by removing unzipped graph files
+    shutil.rmtree(TEMP_UNZIP_FOLDER)
+    os.makedirs(TEMP_UNZIP_FOLDER) # rebuilding temporary dir
