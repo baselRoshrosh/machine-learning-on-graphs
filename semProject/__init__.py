1326c55d3e11a77f09a1d800b0543a5425385189
Leon Behrndt <leon.behrndt@informatik.hu-berlin.de>
2025-03-09 10:20:08 +0100

    fix: remove dot from semProject
    Former-commit-id: 86b8cc1929c494ee55a25bd3ec4ff19dedecf2d4

--- semProject/__init__.py
+++ semProject/__init__.py
@@ -1,4 +1,4 @@
 """Root module of your package"""
-from .semProject import Graph, AttributedDeepwalk, KNN, Topo2Vec
+from semProject import Graph, AttributedDeepwalk, KNN, Topo2Vec
 
 __all__ = ["Graph", "AttributedDeepwalk", "KNN", "Topo2Vec"]
