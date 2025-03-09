3e938a139f7d74082c1c46111b86cf540efd8234
Moritz Spuehler <moritz.spuehler@informatik.hu-berlin.de>
2025-03-09 22:12:49 +0100

    Resolve "Evaluation script einbinden"
    Former-commit-id: 45e7d438c942ab00040c14f883442b9ffdc8a734

--- semProject/__init__.py
+++ semProject/__init__.py
@@ -1,4 +1,10 @@
 """Root module of your package"""
+import sys
+import os
+
+# Add the root directory of your project to the sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from .semProject import Graph, AttributedDeepwalk, KNN, Topo2Vec
 
 __all__ = ["Graph", "AttributedDeepwalk", "KNN", "Topo2Vec"]
