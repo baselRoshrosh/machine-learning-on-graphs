2e701e8a66e09ce8ee56bb699cd185abbb8801ba
Leon Behrndt <leon.behrndt@informatik.hu-berlin.de>
2025-03-09 10:16:15 +0100

    fix: run strategies eval
    Former-commit-id: bccceed4d072420b91bf6ec1fb04812f718113af

--- scripts/run_strategies_for_eval.py
+++ scripts/run_strategies_for_eval.py
@@ -15,7 +15,7 @@ OUTPUT_FOLDER = "./output"
 TEMP_UNZIP_FOLDER = "./temp_unzip"
 
 if not os.path.exists(TEMP_UNZIP_FOLDER):
-        os.makedirs(TEMP_UNZIP_FOLDER)  # Create directory if it doesn't exist 
+    os.makedirs(TEMP_UNZIP_FOLDER)  # Create directory if it doesn't exist 
 
 # Get list of available graphs in ref (without extensions)
 graph_names = [f.split('.')[0] for f in os.listdir(REF_FOLDER) if os.path.isfile(os.path.join(REF_FOLDER, f))]
@@ -24,16 +24,18 @@ graph_names = [f.split('.')[0] for f in os.listdir(REF_FOLDER) if os.path.isfile
 for graph_name in graph_names:
     print(f"Read graph: {graph_name}")
     
-    # etracting graph files
+    # extracting graph files
     with zipfile.ZipFile(os.path.join(INPUT_FOLDER, graph_name + ".zip"), 'r') as zip_ref:
         zip_ref.extractall(TEMP_UNZIP_FOLDER)
         
     # performing strategies
     for i, strategy in enumerate(strategies):
-        print(os.path.join(TEMP_UNZIP_FOLDER, graph_name) + "_features.txt")
+        feature_file = os.path.join(TEMP_UNZIP_FOLDER, graph_name + "_features.txt")
+        edge_file = os.path.join(TEMP_UNZIP_FOLDER, graph_name + "_edges.txt")
+        print(feature_file)
+        
         # 1: getting graph
-        graph = sp.Graph(os.path.join(TEMP_UNZIP_FOLDER, graph_name) + "_features.txt", 
-                         os.path.join(TEMP_UNZIP_FOLDER, graph_name) + "_edges.txt")
+        graph = sp.Graph(feature_file, edge_file)
 
         # 2: initializing strategies
         print(f"performing strategy {strategy_names[i]}")
@@ -44,8 +46,8 @@ for graph_name in graph_names:
         # 4: run and extract
         x.run()
         results = x.extract_results()
-        x.save_features(results, os.path.join(OUTPUT_FOLDER, f"{graph_name}_{strategy_names[i]}txt"))
+        x.save_features(results, os.path.join(OUTPUT_FOLDER, f"{graph_name}_{strategy_names[i]}.txt"))
         
 # Clean up by removing unzipped graph files
 shutil.rmtree(TEMP_UNZIP_FOLDER)
-os.makedirs(TEMP_UNZIP_FOLDER) # rebuilding temporary dir
+os.makedirs(TEMP_UNZIP_FOLDER)  # rebuilding temporary dir
